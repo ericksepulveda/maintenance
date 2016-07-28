df6d6e97d30e05cabb25a095b9a4ca18bb36b92e
ericksepulveda <erick.sepulveda@alumnos.usm.cl>
2016-07-27 21:07:07 -0400

    route to manage facilities

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :facilities
+  get '/management', to: 'facilities#facilities'
   root 'facilities#index'
 end
